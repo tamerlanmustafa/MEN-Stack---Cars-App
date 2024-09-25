8c7a58f8b11ea999e8620e996705c25c0ed8a5a7
Tamerlan Mustafayev <tamerlan.m.2000@gmail.com>
2024-09-25 16:16:00 -0400

    Make new file

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const express = require('express')
 const app = express()
 
 
+
+app.set('view engine', 'ejs')
+
+app.get('/new', )
+
+
 app.listen(3000, () => {
     console.log('Server is running on port: 3000')
-})
+}) 
+
+
+

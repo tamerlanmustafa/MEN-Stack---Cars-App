2c616ca3b947d769622692fed43887d10ef71e4d
Tamerlan Mustafayev <tamerlan.m.2000@gmail.com>
2024-09-25 16:32:00 -0400

    Make form on new page post to create route. Make Create route

--- server.js
+++ server.js
@@ -10,10 +10,14 @@ app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
     res.send("HOME")
 })
+
 app.get('/new', (req, res) => {
     res.render('new')
 } )
 
+app.post('/create', (req, res) => {
+    res.send('Your blog has been successfully created!')
+})
 
 app.listen(3000, () => {
     console.log('Server is running on port: 3000')
